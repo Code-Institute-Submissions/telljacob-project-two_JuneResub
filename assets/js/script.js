a79cff97ff3c5d21d6e96be747b3cd029ca964a1
Jacob <telljacob@gmail.com>
2022-03-12 12:31:28 +0000

    Add initial code to Javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,23 @@
 // Import input data
-const rounds = document.querySelector(".input-rounds");
-const work = document.querySelector(".input-work");
-const rest = document.querySelector(".input-rest");
+let rounds = document.querySelector(".input-rounds");
+let work = document.querySelector(".input-work");
+let rest = document.querySelector(".input-rest");
 
 // Import start, stop and reset buttons
 var start = document.querySelector(".start-workout");
 var stop = document.querySelector(".stop-workout");
 var reset = document.querySelector(".reset-workout");
 
-function setTimer(work) {
-    work = 
-};
 
-function setRound(rounds) {
-    rounds = 
-};
+const countdownEl = document.getElementById("timerCountdown");
+
+setInterval(newCountdown, 1000);
+
+// Timer countdown
+function newCountdown() {
+    
+    countdownEl.innerHTML = '${work}';
+    work--;
+}
+
+// Initiate at Start press and import numbers
