af82f21fc7a639028895b288e802823cf1780e0a
Jacob <telljacob@gmail.com>
2022-03-12 11:47:07 +0000

    Add import of input values to Javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,17 @@
-console.log("Hello from the console!")
+// Import input data
+const rounds = document.querySelector(".input-rounds");
+const work = document.querySelector(".input-work");
+const rest = document.querySelector(".input-rest");
+
+// Import start, stop and reset buttons
+var start = document.querySelector(".start-workout");
+var stop = document.querySelector(".stop-workout");
+var reset = document.querySelector(".reset-workout");
+
+function setTimer(work) {
+    work = 
+};
+
+function setRound(rounds) {
+    rounds = 
+};
