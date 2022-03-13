d682c8676ca79a38ed7e485acdb657c64275e9d4
Jacob <telljacob@gmail.com>
2022-03-13 09:08:45 +0000

    Add functional stop button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,46 @@
 document.getElementById("startWork").onclick = function() {
-    timer()
+    isEnabled = true;
+    timer();
 }
 
 document.getElementById("stopWork").onclick = function() {
-    alert("HELLO SAVE ME FROM")
+    isEnabled = false;
 }
 
 document.getElementById("resetWork").onclick = function() {
-    alert("HELLO SAVE ME FROM HERE")
+    reset();
 }
-
 // Import value from input
-function timer() {
+function timer() {    
     const inputValue = document.getElementById("inputWork").value;
     let counter = inputValue * 60;
     var startcounter = 0;
     setInterval( function(){
+        if (isEnabled) {
         counter--;
-        startcounter++;
-    
-        let hours = Math.floor((counter % (60 * 60 * 24)) / (60 * 60));
-        let minutes = Math.floor((counter % (60 * 60)) / (60));
-        let seconds = Math.floor((counter % 60));
-    
-        if( counter >= 0 ){
-            id = document.getElementById("timerCountdown");
-            id.innerHTML = hours + "h " + minutes + "m " + seconds + "s ";
+            startcounter++;
+        
+            let hours = Math.floor((counter % (60 * 60 * 24)) / (60 * 60));
+            let minutes = Math.floor((counter % (60 * 60)) / (60));
+            let seconds = Math.floor((counter % 60));
+        
+            if( counter >= 0 ){
+                id = document.getElementById("timerCountdown");
+                id.innerHTML = hours + "h " + minutes + "m " + seconds + "s ";
+            }
+        
+            if( counter === 1 ){
+                id.innerHTML = "WORK COMPLETE";
+            }
+        
+            let percentTimer = document.getElementById("percentCountdown");
+            percentTimer.innerHTML = Math.round((startcounter/counter)*100);
         }
-    
-        if( counter === 1 ){
-            id.innerHTML = "WORK COMPLETE";
-        }
-    
-        let percentTimer = document.getElementById("percentCountdown");
-        percentTimer.innerHTML = Math.round((startcounter/counter)*100);
-    
     }, 1000);
 }
 
-function pause() {
-
+function reset() {
+    isEnabled = false;
+    let resetTimer = document.getElementById("timerCountdown").value;
+    resetTimer.innerHTML = "00h:00m:00s";
 }
