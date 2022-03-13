dcaa40c92c7aca2e3251f7436889c5dcc7928caf
Jacob <telljacob@gmail.com>
2022-03-13 02:35:30 +0000

    Add structure of javascript import of variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,25 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button")
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("input-rounds") === "start-workout") {
+                alert("You sent in a message");
+            }
+        })
+    }
+})
+
+
 // Import input data
-let rounds = document.querySelector(".input-rounds");
-let work = document.querySelector(".input-work");
-let rest = document.querySelector(".input-rest");
+let rounds = document.querySelector("input-rounds");
+let work = document.querySelector("input-work");
+let rest = document.querySelector("input-rest");
 
 // Import start, stop and reset buttons
-var start = document.querySelector(".start-workout");
-var stop = document.querySelector(".stop-workout");
-var reset = document.querySelector(".reset-workout");
+var start = document.querySelector("start-workout");
+var stop = document.querySelector("stop-workout");
+var reset = document.querySelector("reset-workout");
 
 
 const countdownEl = document.getElementById("timerCountdown");
